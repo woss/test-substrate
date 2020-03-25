c08fc330f9206dada20c5f2c229f0f892756499a
Daniel Maricic <daniel@woss.io>
2020-03-25 15:38:19 +0100

    init
    Signed-off-by: Daniel Maricic <daniel@woss.io>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -39,6 +39,8 @@ pub use frame_support::{
 
 /// Importing a template pallet
 pub use template;
+pub use poe;
+pub use kitties;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -226,6 +228,14 @@ impl template::Trait for Runtime {
 	type Event = Event;
 }
 
+impl poe::Trait for Runtime {
+	type Event = Event;
+}
+
+impl kitties::Trait for Runtime {
+	type Event = Event;
+}
+
 construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
@@ -242,6 +252,9 @@ construct_runtime!(
 		Sudo: sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Used for the module template in `./template.rs`
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		PoeModule: poe::{Module, Call, Storage, Event<T>},
+		KittiesModule: kitties::{Module, Call, Storage, Event<T>},
+
 	}
 );
 
